875130f6e57bf96c2760ad88345ac36c8f105395
Reka <rekapap28@gmail.com>
2018-10-27 22:47:07 +0100

    db fix

--- db/migrations/01_create_bookmarks_table.sql
+++ db/migrations/01_create_bookmarks_table.sql
@@ -1,4 +1,1 @@
-CREATE DATABASE "bookmark_manager";
 CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR (60));
-CREATE DATABASE "bookmark_manager_test";
-CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
